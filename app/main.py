35192cbbebd5e9575e81db535be38debf72363a4
Fatema-Nawrin <nawrin4000@gmail.com>
2024-12-28 01:41:17 +0600

    echo builtin

--- app/main.py
+++ app/main.py
@@ -5,13 +5,13 @@ def main():
     while True:
         sys.stdout.write("$ ")
 
-        command = input()
+        command = input().strip()
         if command == "exit 0":
             break
         elif command.startswith("echo "):
             print(command[5:])
-            break
-        print(f"{command}: command not found")
+        else:
+            print(f"{command}: command not found")
 
 
 if __name__ == "__main__":
