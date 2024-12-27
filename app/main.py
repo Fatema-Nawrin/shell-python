81c02b4bfdfd780408e4d77839631b0e05bbbf04
Fatema-Nawrin <nawrin4000@gmail.com>
2024-12-28 00:00:35 +0600

    repl

--- app/main.py
+++ app/main.py
@@ -2,13 +2,11 @@ import sys
 
 
 def main():
-    # Uncomment this block to pass the first stage
-    sys.stdout.write("$ ")
+    while True:
+        sys.stdout.write("$ ")
 
-    command = input()
-
-    print(f"{command}: command not found\n")
-    input()
+        command = input()
+        print(f"{command}: command not found\n")
 
 
 if __name__ == "__main__":
